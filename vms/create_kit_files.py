5ffe3100d78a7f52e65f427423b6372ad263369a
Sergey Vorfolomeev <sergey.vorfolomeev@vmssoftware.com>
2023-01-19 17:06:18 +0100

    PYT-206

--- vms/create_kit_files.py
+++ vms/create_kit_files.py
@@ -180,7 +180,7 @@ if __name__ == "__main__":
     major = '3'
     minor = '10'
     level = '0'
-    edit = 'release005'
+    edit = 'release006'
 
     for opt, optarg in opts:
         if opt in ['--type']:
