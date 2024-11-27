2d3cdcc66a643c5f4f216cade2f5cd61ae88e8c7
Sergey Vorfolomeev <sergey.vorfolomeev@vmssoftware.com>
2024-11-27 16:51:52 +0100

    Increment to release009

--- vms/create_kit_files.py
+++ vms/create_kit_files.py
@@ -180,7 +180,7 @@ if __name__ == "__main__":
     major = '3'
     minor = '10'
     level = '0'
-    edit = 'release008'
+    edit = 'release009'
 
     for opt, optarg in opts:
         if opt in ['--type']:
